13b6249ddfe8c5c753fe29f5ec7fd7f68cadf806
Gameidkdmekl <poporaum20@gmail.com>
2025-12-27 00:51:34 +1000

    Update KeySystem.lua

--- Online Script/KeySystem.lua
+++ Online Script/KeySystem.lua
@@ -154,10 +154,10 @@ task.spawn(animateLogo)
 --- Platoboost Key System Configuration ---
 local service = 16094
 local secret = "9bfce86e-a6fc-4baf-93d8-4d77a2254e41"
-local useNonce = true
+local useNonce = false
 
 -- Режим тестирования (для быстрой проверки FREE ключей)
-local TEST_MODE = true
+local TEST_MODE = false
 local TEST_KEYS = {
     ["FREE_ce9cee7b67c4b257da6907af2924dcd2"] = true,
     ["FREE_TEST"] = true,
