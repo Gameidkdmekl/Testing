0deca09b90ec710be0131b557d06465d6bce19f7
Gameidkdmekl <poporaum20@gmail.com>
2026-01-13 23:57:47 +1000

    Update ScriptPC.lua

--- Online Script/ScriptPC.lua
+++ Online Script/ScriptPC.lua
@@ -1,3 +1,4 @@
+game:GetService("Players").LocalPlayer.PlayerGui.DraconicHubGui:Destroy()
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "🔥Draconic Hub X🔥",
     Text = "Welcome Draconic Hub Remake",
